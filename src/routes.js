d2952788c7356e3fee340084ae288a4c87936b81
kartik-raj7 <kartikeyrajgupta007@gmail.com>
2024-01-19 12:11:40 +0530

    minor fix

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,12 @@ import { lazy } from 'react';
 const routes = [
     {
         path: '/',
-        component: lazy(() => import('./pages/homepage')),
+        component: lazy(() => import('./pages/Homepage')),
         exact: true,
       },
   {
     path: '/home',
-    component: lazy(() => import('./pages/homepage')),
+    component: lazy(() => import('./pages/Homepage')),
     exact: true,
     preload: true, 
   },
